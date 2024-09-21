70fea723889260c5a89ff48048e07ae95f6fa278
aallen417 <austin.allen417@gmail.com>
2024-09-21 16:22:34 -0400

    adds about page

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
   return HttpResponse('<h1>Hello, world!</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About the FrogCollector')
