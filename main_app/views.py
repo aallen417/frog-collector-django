5c8de006d16992c00867f828a0318ec7e2dc0916
aallen417 <austin.allen417@gmail.com>
2024-09-21 16:51:16 -0400

    adds about template

--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,4 @@ def home(request):
   return HttpResponse('<h1>Hello, world!</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the FrogCollector')
+  return render(request, 'about.html')
