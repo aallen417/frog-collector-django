a5c478beb5ad905e02252c745883a981be4ce97c
aallen417 <austin.allen417@gmail.com>
2024-09-22 18:06:25 -0400

    adds view functionality to frogs entity

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('frogs/', views.frog_index, name='frog-index'),
+  path('frogs/<int:frog_id>/', views.frog_detail, name='frog-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
+from .models import Frog
 
 # Create your views here.
-
-class Frog:
-  def __init__(self, name, species, description, age):
-    self.name = name
-    self.species = species
-    self.description = description
-    self.age = age
-
-frogs = [
-  Frog('Croaker', 'American Bullfrog', 'Loud', 6),
-  Frog('Hippity', 'Northern Leopard Frog', 'Loves to jump', 3),
-  Frog('Hop', 'Glass Frog', 'Hops between lily pads', 4),
-  Frog('Skrem', 'American Bullfrog', 'Louder', 0),
-]
 
 def home(request):
   return render(request, 'home.html')
@@ -23,4 +10,9 @@ def about(request):
   return render(request, 'about.html')
 
 def frog_index(request):
+  frogs = Frog.objects.all()
   return render(request, 'frogs/index.html', { 'frogs': frogs })
+
+def frog_detail(request, frog_id):
+  frog = Frog.objects.get(id=frog_id)
+  return render(request, 'frogs/detail.html', { 'frog': frog})
