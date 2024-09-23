75796b329cc56a70c1b5afcd5f3f1a804d3c416e
aallen417 <austin.allen417@gmail.com>
2024-09-23 14:20:03 -0400

    adds lilypad model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Frog, Feeding
+from .models import Frog, Feeding, LilyPad
 
 # Register your models here.
 admin.site.register(Frog)
 admin.site.register(Feeding)
+admin.site.register(LilyPad)
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 MEALS = (
@@ -20,6 +21,9 @@ class Frog(models.Model):
   def get_absolute_url(self):
     return reverse('frog-detail', kwargs={'frog_id': self.id})
   
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+  
 class Feeding(models.Model):
   date = models.DateField('Feeding date')
   meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
@@ -31,3 +35,14 @@ class Feeding(models.Model):
   
   class Meta:
     ordering = ['-date']
+
+class LilyPad(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+      return self.name
+  
+  def get_absolute_url(self):
+      return reverse("lilypad-detail", kwargs={"pk": self.id})
+  
