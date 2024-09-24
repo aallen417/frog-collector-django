27e67421b1ff9e833e552c2cdbdef13d1689d184
aallen417 <austin.allen417@gmail.com>
2024-09-23 21:10:39 -0400

    adds sigup, sign in, and logout functionality and protects routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('frogs/', views.frog_index, name='frog-index'),
   path('frogs/<int:frog_id>/', views.frog_detail, name='frog-detail'),
@@ -10,6 +10,8 @@ urlpatterns = [
   path('frogs/<int:pk>/update', views.FrogUpdate.as_view(), name='frog-update'),
   path('frogs/<int:pk>/delete', views.FrogDelete.as_view(), name='frog-delete'),
   path('frogs/<int:frog_id>/add-feeding', views.add_feeding, name='add-feeding'),
+  path('frogs/<int:frog_id>/assoc-lilypad/<int:lilypad_id>/', views.assoc_lilypad, name='assoc-lilypad'),
+  path('accounts/signup', views.signup, name='signup'),
   path('lilypads/create/', views.LilyPadCreate.as_view(), name='lilypad-create'),
   path('lilypads/<int:pk>/', views.LilyPadDetail.as_view(), name='lilypad-detail'),
   path('lilypads/', views.LilyPadList.as_view(), name='lilypad-index'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Frog, LilyPad
 from .forms import FeedingForm
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
@@ -12,15 +17,19 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def frog_index(request):
-  frogs = Frog.objects.all()
+  frogs = Frog.objects.filter(user=request.user)
   return render(request, 'frogs/index.html', { 'frogs': frogs })
 
+@login_required
 def frog_detail(request, frog_id):
   frog = Frog.objects.get(id=frog_id)
+  lilypads_frog_doesnt_have = LilyPad.objects.exclude(id__in = frog.lilypads.all().values_list('id'))
   feeding_form = FeedingForm()
-  return render(request, 'frogs/detail.html', { 'frog': frog, 'feeding_form': feeding_form})
+  return render(request, 'frogs/detail.html', { 'frog': frog, 'feeding_form': feeding_form, 'lilypads': lilypads_frog_doesnt_have})
 
+@login_required
 def add_feeding(request, frog_id):
   form = FeedingForm(request.POST)
   if form.is_valid():
@@ -29,33 +38,58 @@ def add_feeding(request, frog_id):
     new_feeding.save()
   return redirect('frog-detail', frog_id=frog_id)
 
-class FrogCreate(CreateView):
+@login_required
+def assoc_lilypad(request, frog_id, lilypad_id):
+  Frog.objects.get(id=frog_id).lilypads.add(lilypad_id)
+  return redirect('frog-detail', frog_id=frog_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('frog-index')
+    else:
+      error_message ='Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
+
+class FrogCreate(LoginRequiredMixin, CreateView):
   model = Frog
   fields = ['name', 'species', 'description', 'age']
-  success_url = '/frogs/'
+  
+  def form_valid(self, form):
+    form.instance.user = self.request.user 
+    return super().form_valid(form)
 
-class FrogUpdate(UpdateView):
+class FrogUpdate(LoginRequiredMixin, UpdateView):
   model = Frog
   fields = ['species', 'description', 'age']
 
-class FrogDelete(DeleteView):
+class FrogDelete(LoginRequiredMixin, DeleteView):
   model = Frog
   success_url = '/frogs/'
 
-class LilyPadCreate(CreateView):
+class LilyPadCreate(LoginRequiredMixin, CreateView):
   model = LilyPad
   fields = '__all__'
 
-class LilyPadList(ListView):
+class LilyPadList(LoginRequiredMixin, ListView):
   model = LilyPad
 
-class LilyPadDetail(DetailView):
+class LilyPadDetail(LoginRequiredMixin,DetailView):
   model = LilyPad
 
-class LilyPadUpdate(UpdateView):
+class LilyPadUpdate(LoginRequiredMixin, UpdateView):
   model = LilyPad
   fields = ['name', 'color']
 
-class LilyPadDelete(DeleteView):
+class LilyPadDelete(LoginRequiredMixin, DeleteView):
   model = LilyPad
   success_url = '/lilypads/'
+
+class Home(LoginView):
+  template_name = 'home.html'
